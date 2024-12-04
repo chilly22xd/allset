a7d19922ecabe0e84e8ce20b1bda22d4ad1017ea
Josafat Juarez <108589175+chilly22xd@users.noreply.github.com>
2024-12-04 09:50:05 -0600

    nuevo commit

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,10 @@ import Services from "../components/services.vue";
 import Blog from "../components/blog.vue";
 
 const routes = [
-    {path: 'allset/', component: Home},
-    {path: 'allset/about',component: AboutUs},
-    {path: 'allset/services',component: Services},
-    {path: 'allset/blog',component: Blog}
+    {path: '/allset/', component: Home},
+    {path: '/allset/about',component: AboutUs},
+    {path: '/allset/services',component: Services},
+    {path: '/allset/blog',component: Blog}
 ];
 
 const router = createRouter({
