4d729447a14c4c11727f273d31792b10f05ee8db
Josafat Juarez <108589175+chilly22xd@users.noreply.github.com>
2024-12-09 17:32:18 -0600

    btn home animado

--- src/components/home.vue
+++ src/components/home.vue
@@ -5,34 +5,6 @@ import Section3 from './home/Section3.vue';
 import Section4 from './home/Section4.vue';
 import Section5 from './home/Section5.vue';
 
-const masServicios = "our services";
-const cantidadHappy = 800;
-const cantidadAwards = 180
-const arrayCards = [
-  {
-    img: '/icono-team.png',
-    titulo: 'Expert Team',
-    descripcion: 'Our highly trained professionals\n' +
-      'bring a wealth of experience'
-
-  },
-  {
-    img: '/icono-aspiradora.png',
-    titulo: 'Quality Service',
-    descripcion: 'We guarantee a spotless finish that\n' +
-      'exceeds your expectations.'
-
-  },
-  {
-    img: '/icono-sticker.png',
-    titulo: 'Affordable Price',
-    descripcion: 'We offer exceptional services at\n' +
-      'prices that are affordable for\n' +
-      'everyone.'
-
-  }
-]
-
 </script>
 
 <template>
@@ -41,8 +13,6 @@ const arrayCards = [
   <section3 />
   <section4 />
   <section5 />
-
-
 </template>
 
 <style scoped></style>
