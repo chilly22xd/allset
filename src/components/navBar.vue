ee4a99f5630d58f7d7686c74d3984c0281162946
Josafat Juarez <108589175+chilly22xd@users.noreply.github.com>
2024-12-16 14:50:06 -0600

    Fix incorrect asset paths in contact section icons
    Updated the paths for the teléfono and correo icons in Section1.vue to ensure assets are correctly referenced. This resolves potential loading issues for the contact section icons.

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,27 +1,44 @@
 <script setup>
-import {ref, watch} from "vue";
-const isNavOpen = ref(false);
-function openNav() {
-    isNavOpen.value = true;
-}
-function closeNav() {
-    isNavOpen.value = false;
-}
-function toggleNav() {
-    isNavOpen.value = !isNavOpen.value;
+import {onMounted, onBeforeMount, ref} from "vue";
+const arrayStyles = [
+    'position: fixed;',
+    'background-color: #fff;',
+    'box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);',
+    'margin-top: 5px;',
+    'top: 0;',
+    'left: 0;',
+    'right: 0;',
+    'z-index: 10;',
+    'border-radius: 0 0 10px 10px;',
+    'padding: 0.2%;'
+]
+const lastScrollTop = ref(0);
+const navbar = ref(null);
+const handleScroll = () => {
+    /*const cs = window.scrollY;
+    if (navbar.value) {
+        if (!(cs === 0)) {
+            document.querySelector('.header .logo img').style.width = '5vmax';
+            navbar.value.style = arrayStyles.join(' ');
+        } else {
+            navbar.value.style = null;
+            document.querySelector('.header .logo img').style.width = '';
+        }
+    }
+    lastScrollTop.value = cs;*/
 }
 
-watch(isNavOpen, (newValue) => {
-    if(newValue) {
-        document.body.style.overflow = "hidden";
-    } else {
-        document.body.style.overflow = "auto";
-    }
+onMounted(() => {
+    window.addEventListener("scroll", handleScroll);
 })
+onBeforeMount(() => {
+    window.removeEventListener("scroll", handleScroll);
+})
+
 </script>
 <template>
 
-  <header class="header">
+  <header ref="navbar" class="header">
     <div class="logo">
       <img src="/logo.png" alt="logo">
     </div>
@@ -34,24 +51,6 @@ watch(isNavOpen, (newValue) => {
       </ul>
     </nav>
       <router-link class="contact-btn" to="/allset/hireus">Hire Us</router-link>
-      <button class="hamburguer" @click="toggleNav">
-          <div class="line"></div>
-          <div class="line"></div>
-          <div class="line"></div>
-      </button>
-      <div :class="{'nav-active': isNavOpen}" class="menu">
-          <button class="close-btn" @click="closeNav">&times;</button>
-          <div class="menu-links" @click="closeNav">
-              <router-link class="linksa" to="/allset/">Home</router-link>
-              <router-link class="linksa" to="/allset/about">About Us</router-link>
-              <router-link class="linksa" to="/allset/services">Services</router-link>
-              <router-link class="linksa" to="/allset/contact">Contact</router-link>
-              <router-link class="contact-btn" to="/allset/hireus">Hire Us</router-link>
-              <button class="contact-btn">Contact Us</button>
-          </div>
-
-      </div>
-
   </header>
 </template>
 <style scoped>
@@ -123,78 +122,7 @@ watch(isNavOpen, (newValue) => {
     width: 10vmax;
 }
 
-.hamburguer {
-    display: none;
-    flex-direction: column;
-    cursor: pointer;
-    background-color: #3C394E;
-    border-radius: 5px;
-    border: none;
-    padding: 1vmax;
-}
-.hamburguer:focus .line {
-    background-color: #3C394E;
-}
-.hamburguer:focus {
-    background-color: #669FF3;
-}
-
-.hamburguer .line {
-    width: 2.5vmax;
-    height: 0.2vmax;
-    background-color: white;
-    margin: 0.3vmax 0;
-}
-
-.nav-active {
-    display: block !important;
-}
-.menu {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100vmax;
-    background-color: rgba(0, 0, 0, 0.8);
-    display: none;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    z-index: 10;
-}
-.menu .menu-links {
-    height: 100%;
-    display: flex;
-    gap: 2vmax;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-}
-.menu .menu-links .linksa {
-    font-size: 5vmax;
-    font-weight: bold;
-    color: white;
-}
-.menu .menu-links .linksa:hover {
-    color: #669FF3;
-}
-.menu .close-btn {
-    position: absolute;
-    top: 1rem;
-    right: 1rem;
-    background: none;
-    border: none;
-    color: white;
-    font-size: 2rem;
-    cursor: pointer;
-}
-.menu .menu-contact{
-    display: flex;
-    background-color: #669FF3;
-    justify-content: right;
-}
-
-@media (max-width: 768px) {
+@media screen and (max-width: 768px) {
     .header {
         margin: 0 5%;
         display: none;
@@ -204,24 +132,6 @@ watch(isNavOpen, (newValue) => {
     }
     .header .contact-btn {
         display: none;
-    }
-    .hamburguer {
-        display: inherit;
-    }
-    .menu  .contact-btn{
-        margin-top: 2vmax;
-        display: inherit;
-        width: 20vmax;
-        height: 5vmax;
-        align-items: center;
-        justify-content: center;
-    }
-    .menu .contact-btn::before{
-        width: 0;
-        height: 5vmax;
-    }
-    .menu .contact-btn:hover::before{
-        width: 20vmax;
     }
 
 }
